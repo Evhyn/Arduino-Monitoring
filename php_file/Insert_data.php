4f8e03a69f0ee466477d6bd530139d1eb2a8f10f
Evhyn <34357054+Evhyn@users.noreply.github.com>
2017-12-29 18:35:32 +0700

    Add files via upload

---
+++ php_file/Insert_data.php
@@ -0,0 +1,46 @@
+
+/***************************************************************************************
+*    Title: Insert_data_php
+*    Author: Evi Andrianto
+*    Date: 27-Jan-17
+*    Code version: v1.0
+*
+***************************************************************************************/
+
+
+
+
+<?php
+
+
+    // Prepare variables for database connection
+   
+    $username = 'root';  // enter database username
+    $password = '';  // enter database password
+    $host = 'localhost'; 
+    $dbname='data monitoring';
+
+    // Connect to your database
+
+    $con = mysqli_connect($host, $username, $password, $dbname);
+
+    // Check connection
+    if ($con->connect_error) {
+    	die("Connection failed: " . $con->connect_error);
+	}
+
+
+    // Prepare the SQL statement
+
+    $sql = "INSERT INTO dht11 (temp,humidity) VALUES ('".$_GET["value1"]."',".$_GET["value2"].")";    
+
+    // Execute SQL statement
+
+    if ($con->query($sql) === TRUE) {
+    	echo "New record created successfully";
+    } else {
+    	echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+
+$con->close();
+?>
